26baccc74c8fa52829de47833043ed6154a99f8c
Azba Bhavnagari <azbaashfaq5@gmail.com>
2021-08-21 02:25:22 +0530

    Update assesment.py

--- assesment.py
+++ assesment.py
@@ -9,60 +9,64 @@ path=(r'C:\Users\azba\OneDrive\Desktop\Azba\Incubyte\incubyte\Records.accdb')
 #connecting into the database
 connect= pyodbc.connect(driver=drive, DBQ=path , autocommit=True)
 cursor=connect.cursor()
-print("========================================================")
-print("Enter the your choice:")
-print("1)Add record\n2)Display records")
-choice=int(input())
-print("========================================================")
-if choice==1:
-        #getting the input for the tables
-        c_name=input("Enter the name of the customer:")
-        c_id=input("Enter the customer id:")
-        c_opendate=input("Enter the Customer open date:")
-        lastconsult=input("Enter the Last consult date:")
-        vax=input("Enter the Customer vaccine:")
-        doc=input("Enter the doctor cunsulted:")
-        state=input("Enter the Customer State:")
-        country=input("Enter the Customer Country:")
-        pin=input("Enter the pincode:")
-        dob=input("Enter the date of birth:")
-        active=input("Enter whether the Customer is active or not:");
-        val=(c_name,c_id,c_opendate,lastconsult,vax,doc,state,country,pin,dob,active)
-        if  country=="USA" or country=="usa":
-            cursor.execute("INSERT INTO USA_db VALUES (?,?,?,?,?,?,?,?,?,?,?)",val)
-            cursor.commit()
-            print("Record inserted succesfully!")
-        elif country=="in" or country=="IN":
-            cursor.execute("INSERT INTO IN_db VALUES (?,?,?,?,?,?,?,?,?,?,?)",val)
-            cursor.commit()
-            print("Record inserted succesfully!")
+
+session=1
+while session==1:
+    print("========================================================")
+    print("Enter the your choice:")
+    print("1)Add record\n2)Display records")
+    choice=int(input())
+    print("========================================================")
+    if choice==1:
+            #getting the input for the tables
+            c_name=input("Enter the name of the customer:")
+            c_id=input("Enter the customer id:")
+            c_opendate=input("Enter the Customer open date:")
+            lastconsult=input("Enter the Last consult date:")
+            vax=input("Enter the Customer vaccine:")
+            doc=input("Enter the doctor cunsulted:")
+            state=input("Enter the Customer State:")
+            country=input("Enter the Customer Country:")
+            pin=input("Enter the pincode:")
+            dob=input("Enter the date of birth:")
+            active=input("Enter whether the Customer is active or not:");
+            val=(c_name,c_id,c_opendate,lastconsult,vax,doc,state,country,pin,dob,active)
+            if  country=="USA" or country=="usa":
+                cursor.execute("INSERT INTO USA_db VALUES (?,?,?,?,?,?,?,?,?,?,?)",val)
+                cursor.commit()
+                print("Record inserted succesfully!")
+            elif country=="in" or country=="IN":
+                cursor.execute("INSERT INTO IN_db VALUES (?,?,?,?,?,?,?,?,?,?,?)",val)
+                cursor.commit()
+                print("Record inserted succesfully!")
+                    
+            else:
+                print("Sorry NO branches in",country)
+                
+    elif choice==2:
+        print("Which county records you want to display?")
+        print("1)USA")
+        print("2)India")
+        ch=int(input())
+        if ch==1:
+            cursor.execute('select * from USA_db')
+            for row in cursor.fetchall():
+                for x in row:
+                    print(x,"\t|",end="")
+                print()
+        elif ch==2:
+            cursor.execute('select * from IN_db')
+            for row in cursor.fetchall():
+                for x in row:
+                    print(x,"\t|",end="")
+                print()
                 
         else:
-            print("Sorry NO branches in",country)
-            
-elif choice==2:
-    print("Which county records you want to display?")
-    print("1)USA")
-    print("2)India")
-    ch=int(input())
-    if ch==1:
-        cursor.execute('select * from USA_db')
-        for row in cursor.fetchall():
-            for x in row:
-                print(x,"\t|",end="")
-            print()
-    elif ch==2:
-        cursor.execute('select * from IN_db')
-        for row in cursor.fetchall():
-            for x in row:
-                print(x,"\t|",end="")
-            print()
-            
+            print("Invalid input!")
+                
     else:
-        print("Invalid input!")
-            
-else:
-    print("We dont provide sevices in",country)
+        print("We dont provide sevices in",country)
+    session=int(input("Do you want to end the session?[0/1]"))
     
 
 
